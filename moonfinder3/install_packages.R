b4bf05ff637c77cc6d3580883bffbe9d656df025
logan <yuelong.chen.btr@gmail.com>
2025-07-17 15:38:49 +0800

    build(moonfinder3): add Docker setup and package configuration files
    add Dockerfile, package lists, and GitHub workflow for moonfinder3 environment

---
+++ moonfinder3/install_packages.R
@@ -0,0 +1,38 @@
+setRepositories(ind = 1:3, addURLs = c('https://satijalab.r-universe.dev', 'https://bnprks.r-universe.dev/'))
+args <- commandArgs(trailingOnly = FALSE)
+script_path <- sub("--file=", "", args[grep("--file=", args)])
+script_dir <- dirname(script_path)
+
+base_packages <- scan(file.path(script_dir, "CRAN_packages.txt"), what="", sep="\n")
+bioc_packages <- scan(file.path(script_dir, "bioconductor_packages.txt"), what="", sep="\n")
+github_packages <-  read.table(file.path(script_dir, "devtools_packages.txt"), 
+                               header = FALSE, 
+                               stringsAsFactors = FALSE)
+
+
+
+
+
+tryCatch({
+    install.packages(base_packages)
+}, error = function(e) {
+    message('CRAN R packages: ', conditionMessage(e))
+    quit(status = 1)
+})
+
+
+tryCatch({
+    BiocManager::install(bioc_packages)
+}, error = function(e) {
+    message('Bioconductor: ', conditionMessage(e))
+    quit(status = 1)
+})
+
+tryCatch({
+    lapply(github_packages$V1, function(pkg) devtools::install_github(pkg))
+}, error = function(e) {
+    message('github R packages: ', conditionMessage(e))
+    quit(status = 1)
+})
+
+IRkernel::installspec(name = 'VSCODE_R', displayname = 'VSCODE_R', user = FALSE)
